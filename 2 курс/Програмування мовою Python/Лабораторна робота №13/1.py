14716d2494960b0232cbf8afdd8e85369d55cd50
xxehwuq <xxehwuq@gmail.com>
2024-11-20 18:31:47 +0100

    -

--- 2 курс/Програмування мовою Python/Лабораторна робота №13/1.py
+++ 2 курс/Програмування мовою Python/Лабораторна робота №13/1.py
@@ -5,19 +5,18 @@
 # вставте легенду. Використайте бібліотеку Matplotlib.
 # Y(x)=sin(10*x)*sin(3*x)/(x^2), x=[0...4]
 
-import math
+import numpy as np
 import matplotlib.pyplot as plot
 
-x_values = [i/100 for i in range(1, 400)]
+x = np.linspace(0.01, 4, 100)
+y = np.sin(10 * x) * np.sin(3 * x) / (x**2)
 
-y_values = [math.sin(10 * x) * math.sin(3 * x) / (x**2) for x in x_values]
-
-plot.figure(figsize=(7, 7))
-plot.plot(x_values, y_values, linestyle='solid', color='red', linewidth=2, label="Y(x)=sin(10*x)*sin(3*x)/(x^2)")
+plot.figure(figsize=(8, 8))
+plot.plot(x, y, linestyle='solid', color='blue', linewidth=2, label="Y(x)=sin(10*x)*sin(3*x)/(x^2), x=[0...4]")
 
 plot.xlabel("x")
-plot.ylabel("y")
-plot.title("Y(x)=sin(10*x)*sin(3*x)/(x^2)", fontsize=14)
+plot.ylabel("y(x)")
+plot.title("Y(x)=sin(10*x)*sin(3*x)/(x^2), x=[0...4]", fontsize=14)
 
 plot.legend()
 plot.grid(alpha=0.5)
